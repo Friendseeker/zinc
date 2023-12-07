5d091ef918d2191ae6bb10bc39434e756f6fbd81
friendseeker <66892505+Friendseeker@users.noreply.github.com>
2023-12-07 12:00:11 -0800

    Document some findings

--- zinc/src/sbt-test/source-dependencies/default-namespace-implicit/changes/Foo.scala
+++ zinc/src/sbt-test/source-dependencies/default-namespace-implicit/changes/Foo.scala
@@ -1,5 +1,6 @@
-import sbt.zinc.example._
-
+import sbt.zinc.example.Baz
+// Adding the below import makes issue to changes/Foo.scala and Foo.scala make issue go away
+// import `package`.b
 class Foo(implicit baz: Baz)
 
 object Foo {
